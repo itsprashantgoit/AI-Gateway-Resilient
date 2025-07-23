366473e5895b7d8deab0ace06f3be1761a9b49ba
Prashant Goit <itsprashantgoit@gmail.com>
2025-07-23 01:18:09 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/gateway/models.ts
+++ src/components/gateway/models.ts
@@ -9,7 +9,6 @@ export const models: Model[] = [
     { name: "Deepseek R1 0528 (Chat)", id: "deepseek-ai/DeepSeek-R1-Distill-Llama-70B", type: "chat" },
     { name: "AFM-4.5B-Preview (Chat)", id: "arcee-ai/AFM-4.5B-Preview", type: "chat" },
     { name: "Meta Llama Vision Free (Chat) - DEPRECATED", id: "meta-llama/Llama-Vision-Free", type: "chat" },
-    { name: "DeepSeek R1 Distill Llama 70B (Chat)", id: "deepseek-ai/DeepSeek-R1-Distill-Llama-70B", type: "chat" },
     { name: "EXAONE Deep 32B (Chat)", id: "lgai/exaone-deep-32b", type: "chat" },
     { name: "EXAONE 3.5 32B Instruct (Chat)", id: "lgai/exaone-3-5-32b-instruct", type: "chat" },
     { name: "Meta Llama 3.3 70B (Chat) - DEPRECATED", id: "meta-llama/Llama-3.3-70B-Instruct-Turbo-Free", type: "chat" },
